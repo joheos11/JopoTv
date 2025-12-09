19087c57aa3ac38507d100584741d950d1c3e8fa
joheos11 <joheos11@gmail.com>
2025-12-09 21:05:44 +0100

    Actualiza las URLs de las listas M3U en el complemento

--- addon.js
+++ addon.js
@@ -7,9 +7,8 @@ const builder = new addonBuilder(manifest);
 
 // URLs de las listas
 const LISTAS_M3U = {
-    "tv_tdt": "https://iptv-org.github.io/iptv/countries/es.m3u",
-    "tv_pluto": "https://i.mjh.nz/PlutoTV/es.m3u8",
-    "tv_iptv_org": "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlists/playlist_spain.m3u8"
+    "tv_tdtchannels": "https://www.tdtchannels.com/lists/tv.m3u",
+    "tv_elcano": "https://ipfs.io/ipns/k2k4r8oqlcjxsritt5mczkcn4mmvcmymbqw7113fz2flkrerfwfps004/data/listas/lista_fuera_iptv.m3u"
 };
 
 // --- FUNCIONES AUXILIARES ---
